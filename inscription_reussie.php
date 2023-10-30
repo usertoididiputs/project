e7ea098d243b48496ef567ad3fedd35af2049c3f
usertoididiputs <148789842+usertoididiputs@users.noreply.github.com>
2023-10-30 17:28:33 +0100

    Add files via upload

---
+++ inscription_reussie.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inscription Réussie</title>
+</head>
+<body>
+    <h1>Inscription Réussie</h1>
+    <p>Merci de vous être inscrit en tant que candidat. Votre inscription a été effectuée avec succès.</p>
+    <p>Vous pouvez maintenant vous connecter avec vos identifiants.</p>
+    <a href="connexion_candidat.php">Se connecter</a>
+</body>
+</html>
