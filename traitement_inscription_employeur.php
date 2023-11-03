5bee5b3ada4c12920baf8f6808c4cf190e7a844c
usertoididiputs <148789842+usertoididiputs@users.noreply.github.com>
2023-11-03 20:29:37 +0100

    Add files via upload

---
+++ traitement_inscription_employeur.php
@@ -0,0 +1,34 @@
+<?php
+// Connexion à la base de données
+$mysqli = new mysqli("localhost", "root", "", "can_emp");
+
+// Vérification de la connexion
+if ($mysqli->connect_error) {
+    die("Échec de la connexion à la base de données : " . $mysqli->connect_error);
+}
+
+// Récupération des données soumises par le formulaire
+$nom_entreprise = $_POST['nom_entreprise'];
+$email_entreprise = $_POST['email_entreprise'];
+$mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_BCRYPT);
+$secteur_activite = $_POST['secteur_activite'];
+$adresse = $_POST['adresse'];
+$telephone = $_POST['telephone'];
+
+// Requête d'insertion des données dans la table "employeurs"
+$sql = "INSERT INTO employeurs (nom_de_l_entreprise, email, mot_de_passe, secteur_d_activite, adresse, numero_de_telephone) VALUES (?, ?, ?, ?, ?, ?)";
+
+$insert_stmt = $mysqli->prepare($sql);
+$insert_stmt->bind_param("ssssss", $nom_entreprise, $email_entreprise, $mot_de_passe, $secteur_activite, $adresse, $telephone);
+
+if ($insert_stmt->execute()) {
+    // Redirection vers une page de succès ou autre
+    header("Location: inscription_employeur_reussie.php");
+} else {
+    // Gestion de l'erreur
+    echo "Erreur : " . $mysqli->error;
+}
+
+// Fermeture de la connexion
+$mysqli->close();
+?>
