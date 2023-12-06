ab08d4162070d7bc27bc353b841553a5c8128e65
usertoididiputs <148789842+usertoididiputs@users.noreply.github.com>
2023-12-06 17:59:44 +0100

    Update recherche_candidats.php

--- recherche_candidats.php
+++ recherche_candidats.php
@@ -1,3 +1,68 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "can_emp");
+
+if ($mysqli->connect_error) {
+    die("Échec de la connexion à la base de données : " . $mysqli->connect_error);
+}
+
+// Assurez-vous que la session est démarrée
+session_start();
+
+// Vérifiez si l'employeur est connecté
+if (!isset($_SESSION['id_employeur'])) {
+    // L'employeur n'est pas connecté, redirigez vers la page de connexion
+    header("Location: connexion.php");
+    exit;
+}
+
+$id_employeur = $_SESSION['id_employeur'];
+
+$query = "SELECT nom_de_l_entreprise, email, secteur_d_activite, adresse, numero_de_telephone FROM employeurs WHERE id = ?";
+$stmt = $mysqli->prepare($query);
+$stmt->bind_param("i", $id_employeur);
+$stmt->execute();
+$stmt->bind_result($nom_entreprise, $email, $secteur_activite, $adresse, $telephone);
+$stmt->fetch();
+$stmt->close();
+
+// Définissez des variables pour stocker les résultats de la recherche
+$resultats = array();
+
+// Vérifiez si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérez les critères de recherche du formulaire
+    $nom_candidat = $_POST['nom_candidat'] ?? '';
+    $competence = $_POST['competence'] ?? '';
+    $experience = $_POST['experience'] ?? 0; // Assurez-vous que le champ expérience est un nombre
+
+    // Si le bouton "Trouver un profil" est cliqué
+    if (isset($_POST['trouver_profil'])) {
+        // Récupérez la recherche par compétences
+        $recherche_competences = $_POST['recherche_competences'] ?? '';
+
+        // Construisez la requête SQL pour la recherche par compétences
+        $query = "SELECT * FROM candidats WHERE competences LIKE ?";
+        $stmt = $mysqli->prepare($query);
+        $recherche_competences_param = "%{$recherche_competences}%";
+        $stmt->bind_param("s", $recherche_competences_param);
+        $stmt->execute();
+        $resultats = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
+    } else {
+        // Construisez la requête SQL pour la recherche des candidats
+        $query = "SELECT * FROM candidats WHERE nom LIKE ? AND competences LIKE ? AND experiences_professionnelles >= ?";
+        $stmt = $mysqli->prepare($query);
+        $nom_candidat_param = "%{$nom_candidat}%";
+        $competence_param = "%{$competence}%";
+        $stmt->bind_param("ssi", $nom_candidat_param, $competence_param, $experience);
+        $stmt->execute();
+        $resultats = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
+    }
+}
+$mysqli->close();
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -34,7 +99,7 @@
             border-radius: 8px;
             box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
             padding: 20px;
-            width: 80%;
+            width: 400px;
             text-align: center;
             transition: transform 0.3s;
             margin-top: 20px;
@@ -98,36 +163,31 @@
             box-sizing: border-box;
         }
 
-        /* Style pour masquer la section des résultats par défaut */
+        /* Style pour masquer la section des résultats */
         .resultats-container {
-            display: none;
+            display: <?php echo (!empty($resultats)) ? 'block' : 'none'; ?>;
             margin-top: 20px;
         }
 
-        /* Style pour les résultats */
         .resultats {
-            border-collapse: collapse;
-            width: 100%;
-            margin-top: 20px;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
         }
 
-        .resultats th, .resultats td {
+        .candidat {
+            background-color: #fff;
             border: 1px solid #ddd;
-            padding: 12px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            padding: 15px;
+            margin: 10px;
+            width: 200px;
             text-align: left;
         }
 
-        .resultats th {
-            background-color: #007bff;
-            color: white;
-        }
-
-        .resultats tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-
-        .resultats tr:hover {
-            background-color: #ddd;
+        .candidat p {
+            margin: 5px 0;
         }
     </style>
 </head>
@@ -155,8 +215,14 @@
             <label for="experience">Expérience (en années) :</label>
             <input type="number" name="experience" min="0">
 
+            <!-- Nouveau champ pour la recherche par compétences -->
+            <label for="recherche_competences">Rechercher par compétences :</label>
+            <input type="text" name="recherche_competences">
+
             <input type="submit" value="Rechercher des candidats">
             <input type="reset" value="Réinitialiser" onclick="resetForm()">
+            <!-- Nouveau bouton pour trouver un profil par compétences -->
+            <input type="submit" name="trouver_profil" value="Trouver un profil">
         </form>
 
         <!-- Section des résultats -->
@@ -165,19 +231,18 @@
             // Affichez les résultats de la recherche
             if (!empty($resultats)) {
                 echo "<h2>Résultats de la recherche</h2>";
-                echo "<table class='resultats'>";
-                echo "<tr><th>Nom</th><th>Prénom</th><th>Email</th><th>CV</th><th>Compétences</th><th>Expériences professionnelles</th></tr>";
+                echo "<div class='resultats'>";
                 foreach ($resultats as $resultat) {
-                    echo "<tr>";
-                    echo "<td>" . $resultat['nom'] . "</td>";
-                    echo "<td>" . $resultat['prenom'] . "</td>";
-                    echo "<td>" . $resultat['email'] . "</td>";
-                    echo "<td>" . $resultat['cv'] . "</td>";
-                    echo "<td>" . $resultat['competences'] . "</td>";
-                    echo "<td>" . $resultat['experiences_professionnelles'] . " années</td>";
-                    echo "</tr>";
+                    echo "<div class='candidat'>";
+                    echo "<p><strong>Nom:</strong> " . $resultat['nom'] . "</p>";
+                    echo "<p><strong>Prénom:</strong> " . $resultat['prenom'] . "</p>";
+                    echo "<p><strong>Email:</strong> " . $resultat['email'] . "</p>";
+                    echo "<p><strong>CV:</strong> " . $resultat['cv'] . "</p>";
+                    echo "<p><strong>Compétences:</strong> " . $resultat['competences'] . "</p>";
+                    echo "<p><strong>Expériences professionnelles:</strong> " . $resultat['experiences_professionnelles'] . " années</p>";
+                    echo "</div>";
                 }
-                echo "</table>";
+                echo "</div>";
             }
             ?>
         </div>
